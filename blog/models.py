60943d672e02f57b208a0c391dfcebc624b49cca
Yasenko_Lev <l.study.kpi@gmail.com>
2021-03-28 13:18:26 +0300

    model: comment add

--- blog/models.py
+++ blog/models.py
@@ -18,3 +18,21 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comment")
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return f"comment: {self.title}"
+
